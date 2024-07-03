b1bba8711f539734119f81ad0a9a486ac754f991
Giorgio Luciano <giorgio.luciano@gmail.com>
2024-07-03 16:31:47 +0200

    update

--- ReSpect_v2/Gstar/LevenMarq.r
+++ ReSpect_v2/Gstar/LevenMarq.r
@@ -98,25 +98,8 @@ kernel <- function(H, w, s) {
 }
 
 
-kernelD <- function(H, w, s) {
-  ns <- length(s)
-  hs <- numeric(ns)
-  hs[1] <- 0.5 * log(s[2] / s[1])
-  hs[ns] <- 0.5 * log(s[ns] / s[ns - 1])
-  hs[2:(ns - 1)] <- 0.5 * (log(s[3:ns]) - log(s[1:(ns - 2)]))
-  
-  n <- length(w)
-  
-  ws <- outer(w, s, "*")
-  ws2 <- ws^2
-  Hsuper <- matrix(exp(H), nrow = 2 * n, ncol = ns, byrow = TRUE) * rep(hs, each = 2 * n)
-  
-  DK <- rbind(ws2 / (1 + ws2), ws / (1 + ws2)) * Hsuper
-  
-  return(DK)
-}
-
 GetResidualJacobian <- function(pf, L, Gst, H, w, s) {
+  
   n <- length(w)
   ns <- length(s)
   nl <- ns - 2
